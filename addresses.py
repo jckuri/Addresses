16c1726b2564f5733d518ba5e129e4f1d2392680
jckuri <jckuri@gmail.com>
2022-03-15 01:54:18 -0500

    new commit

--- addresses.py
+++ addresses.py
@@ -14,6 +14,37 @@ def read_allowed_zips():
  return lines2
 
 
+def read_saved_addresses():
+ f = open('saved_addresses.txt', 'r')
+ lines = f.readlines() 
+ f.close()
+ lines2 = []
+ for line in lines:
+  lines2.append(line.strip())
+ return lines2
+ 
+
+def write_addresses(addresses):
+ f = open('saved_addresses.txt', 'w')
+ for address in addresses:
+  f.write(address + '\n')
+ f.close()
+ 
+ 
+def add_address(address):
+ addresses = read_saved_addresses()
+ if not address in addresses:
+  addresses.append(address)
+  write_addresses(addresses)
+  
+  
+def delete_address(address):
+ addresses = read_saved_addresses()
+ if address in addresses:
+  addresses.remove(address)
+  write_addresses(addresses)
+  
+
 class GoogleAPI:
 
 
@@ -42,6 +73,8 @@ class GoogleAPI:
   return response.json()
 
 
+LIST_WIDTH = 50
+
 class AddressWindow:
 
  def __init__(self):
@@ -57,19 +90,23 @@ class AddressWindow:
   sv.trace("w", lambda name, index, mode, sv = sv: self.entry_modified(sv))
   self.input = tk.Entry(self.frame, textvariable = sv)
   self.input.pack()
+  self.input.config(width = LIST_WIDTH)
   
   self.list = tk.Listbox(self.frame, name = 'listbox')
   self.list.pack()
   self.list.bind('<<ListboxSelect>>', self.on_select)
+  self.list.config(width = LIST_WIDTH)
   
   self.label2 = tk.Label(self.frame, text = "Your saved addresses:")
   self.label2.pack()
   
   self.list2 = tk.Listbox(self.frame, name = 'listbox2')
   self.list2.pack()
-  self.list2.bind('<<ListboxSelect>>', self.on_select2)
+  self.list2.bind("<BackSpace>", self.delete_saved_address)
+  self.list2.config(width = LIST_WIDTH)
   
   self.maps = GoogleAPI()
+  self.refresh_saved_addresses()
 
   self.thread = threading.Thread(target = self.counter_thread)
   self.thread.start()
@@ -86,7 +123,7 @@ class AddressWindow:
  def center_window(self):
   self.frame.resizable(False, False)
   window_width = 400
-  window_height = 200
+  window_height = 400
   screen_width = self.frame.winfo_screenwidth()
   screen_height = self.frame.winfo_screenheight()
   x_cordinate = int((screen_width - window_width) / 2)
@@ -110,14 +147,30 @@ class AddressWindow:
   allowed_zip = postal_code in self.allowed_zips
   allowed_string = 'ALLOWED' if allowed_zip else 'NOT ALLOWED'
   print('postal_code={} ({})'.format(postal_code, allowed_string))
+  if allowed_zip:
+   add_address(place_id)
+   self.refresh_saved_addresses()
+   
+   
+ def refresh_saved_addresses(self):
+  self.list2.delete(0, tk.END)  
+  addresses = read_saved_addresses()
+  n = len(addresses)
+  for i in range(n):
+   place_id = addresses[i]
+   json = self.maps.call_place_details(place_id)
+   address_name = json['result']['name']
+   self.list2.insert(i, address_name)
   
   
- def on_select2(self, event):
+ def delete_saved_address(self, event):
   widget = event.widget
   if len(widget.curselection()) == 0: return
   index = int(widget.curselection()[0])
-  value = widget.get(index)
-  print('index={}, value={}'.format(index, value))
+  addresses = read_saved_addresses()
+  address = addresses[index]
+  delete_address(address)
+  self.refresh_saved_addresses()
   
  
  def entry_modified(self, sv):
