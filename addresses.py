031010c1fd0a8a5057d6e62bca45aa4e791f2eec
jckuri <jckuri@gmail.com>
2022-03-14 21:57:45 -0500

    new commit

--- addresses.py
+++ addresses.py
@@ -6,8 +6,14 @@ class AddressWindow:
  def __init__(self):
   self.frame = tk.Tk()
   self.frame.title("Addresses")
-  self.frame.geometry('400x200')
-  self.input = tk.Text(self.frame, height = 5, width = 20)
+  self.center_window()
+  
+  #self.input = tk.Text(self.frame, height = 5, width = 20)
+  #self.input = tk.Entry(self.frame)
+  
+  sv = tk.StringVar()
+  sv.trace("w", lambda name, index, mode, sv = sv: self.entry_modified(sv))
+  self.input = tk.Entry(self.frame, textvariable = sv)
   self.input.pack()
   
   self.list = tk.Listbox(self.frame, name = 'listbox')
@@ -17,23 +23,41 @@ class AddressWindow:
   self.list.pack()
   self.list.bind('<<ListboxSelect>>', self.on_select)
   
-  self.button = tk.Button(self.frame, text = "Print", command = self.print_input)
-  self.button.pack()
+  #self.button = tk.Button(self.frame, text = "Print", command = self.print_input)
+  #self.button.pack()
+  
   self.label = tk.Label(self.frame, text = "")
   self.label.pack()
   self.frame.mainloop()
 
   
- def print_input(self):
-  text = self.input.get(1.0, "end-1c")
-  self.label.config(text = "Provided Input: " + text)
+ #def print_input(self):
+  ##text = self.input.get(1.0, "end-1c")
+  #text = self.input.get()
+  #self.label.config(text = "Provided Input: " + text)
+
+  
+ def center_window(self):
+  self.frame.resizable(False, False)
+  window_height = 500
+  window_width = 900
+  screen_width = self.frame.winfo_screenwidth()
+  screen_height = self.frame.winfo_screenheight()
+  x_cordinate = int((screen_width - window_width) / 2)
+  y_cordinate = int((screen_height - window_height) / 2)
+  self.frame.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
 
   
  def on_select(self, event):
-  w = event.widget
-  index = int(w.curselection()[0])
-  value = w.get(index)
+  widget = event.widget
+  index = int(widget.curselection()[0])
+  value = widget.get(index)
   print('You selected item {}: "{}"'.format(index, value))
+  
+ 
+ def entry_modified(self, sv):
+  text = sv.get()
+  self.label.config(text = "Modified entry: " + text)
 
 
 def main():
